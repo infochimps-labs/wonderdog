c2070a7c56c748b67c2f6545539c9da296896437
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-25 17:36:52 -0500

    changes to try to get it to work, still not working possibly because I'm making it use too much memory

--- test/cardinality.rb
+++ test/cardinality.rb
@@ -3,26 +3,41 @@ require 'configliere'
 require 'json'
 require 'multi_json'
 
-Settings.use :commandline
-Settings.use :config_block
-Settings.define :dump
-Settings.define :field
-Settings.resolve!
+#Settings.use :commandline
+#Settings.use :config_block
+#Settings.define :dump
+#Settings.define :field
+#Settings.resolve!
 
-@fields = {}
-def get_value_counts(dump)
-  File.open(dump).each do |line|
-    record = MultiJson.load(line)
-    record.keys.each do |field|
-      @fields[field] ||= Hash.new(0)
-      @fields[field][record[field]] ||= Hash.new(0)
-      @fields[field][record[field]] += 1
+
+class Cardinality
+  attr_accessor :fields
+
+  def initialize(dump)
+    @dump = dump
+    @fields = {}
+  end
+
+  def get_value_counts
+    File.open(@dump).each do |line|
+      record = MultiJson.load(line)
+      record.keys.each do |field|
+        @fields[field] ||= Hash.new(0)
+        @fields[field][record[field]] ||= Hash.new(0)
+        @fields[field][record[field]] += 1
+      end
+    end
+    puts @fields.inspect
+  end
+
+  def output
+    @field.keys.each do |field|
+      puts "#{field} has #{@fields[field].keys.size} values"
     end
   end
 end
 
-get_value_counts(Settings.dump)
-puts @field_names.inspect
-@field.keys.each do |field|
-  puts @fields[field].keys.size
-end
+#card_ob = Cardinality.new("/home/missy/GitProjects/wonderdog/test/flight_count_20130405").get_value_counts
+#puts card_ob.fields.inspect
+
+
