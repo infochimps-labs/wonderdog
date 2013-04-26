f27894f71e7480c8c253088832473614072ac329
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-26 11:31:58 -0500

    SQAR! now can change an index's settings and clear caches

--- sqar/sqar.rb
+++ sqar/sqar.rb
@@ -2,41 +2,44 @@
 ## squirrel => Standard Query Ultracrepidate Iamatology Ruby Resource for Elasticsearch Labarum ##
 
 
+
 require "configliere"
 require_relative "../test/esbackup_stripped.rb"
 require_relative "../test/cardinality.rb"
 require_relative "../sqar/replay.rb"
 require_relative "../test/warmer_interface.rb"
+require_relative "../test/clear_es_caches.rb"
+require_relative "../test/change_es_index_settings.rb"
 
 Settings.use :commandline
 Settings.use :config_block
-Settings.define :output_dir,            default: '',            description: 'Directory to put output, defaults to current path'
-Settings.define :dump_file,             default: nil,           description: 'The name of the dumpfile to use, default is nil'
-Settings.define :dump_index,            default: nil,           description: 'Create dump of the given index, default is nil'
-Settings.define :query,                 default: nil,           description: 'Query to use in order to limit the data extracted from the index, default nil'
-Settings.define :restore_index,         default: nil,           description: 'Restore the given index from --dump_file, default is nil'
-Settings.define :create_index,          default: nil,           description: 'Create an index of the given name, default is nil'
-Settings.define :duplicate_index,       default: nil,           description: 'Duplicate the given index, defaults to nil'
-Settings.define :restore_index,         default: nil,           description: 'Restore the given index, defaults to nil'
-Settings.define :cardinality,           default: nil,           description: 'Return the cardinality of the given index, defaults to nil'
-Settings.define :cache,                 default: true,          description: 'Use cache, defaults to true'
-Settings.define :temperature,           default: 'cold',        description: 'Hot or Cold cache, cold cache will clear cache before running queries, defaults to cold'
-Settings.define :warmers,               default: false,         description: 'Use warmers, defaults to false'
-Settings.define :new_warmers_name,      default: nil,           description: 'Name of warmer to create, defaults to nil'
-Settings.define :create_warmer,         default: nil,           description: 'Query to create warmer, defaults to nil'
-Settings.define :remove_warmer,         default: nil,           description: 'Name of warmer to remove, defaults to nil'
-Settings.define :refresh_interval,      default: -1,            description: 'Set indexes cache refresh level, defaults to -1'
-Settings.define :execute_slow_queries,  default: nil,           description: 'Execute the slow log queries in the given log file, defaults to nil'
-Settings.define :batch_size,            default: 100,           description: 'Batch size when processing gzip file, defaults to 100'
-Settings.define :dump_mapping,          default: nil,           description: 'The name of the file in which to dump the indexes mapping, defaults to nil'
-Settings.define :restore_mapping,       defualt: nil,           description: 'The mapping json file to use when restoring the index, defaults to nil'
-Settings.define :duplicate_mapping,     default: nil,           description: 'The mapping json file to use when duplicating documents in an index, defaults to nil'
-Settings.define :host,                  default: nil,           description: 'The elasticsearch hostname, defaults to nil'
-Settings.define :port,                  default: 9200,          description: 'The port on the elasticsearch host to connect to, defaults to 9200'
-Settings.define :logfile,               default: nil,           description: 'Logfile to use for slow logs, default nil'
-Settings.define :clear_all_cache,       default: false,         description: 'Clear all caches, defaults to false'
-Settings.define :clear_filter_cache,    default: false,         description: 'Clear filter cache, defaults to false'
-Settings.define :clear_fielddata,       default: false,         description: 'Clear fielddata, defaults to false'
+Settings.define :output_dir,                                default: '',    description: 'Directory to put output, defaults to current path'
+Settings.define :dump_file,                                 default: nil,   description: 'The name of the dumpfile to use, default is nil'
+Settings.define :dump_index,                                default: nil,   description: 'Create dump of the given index, default is nil'
+Settings.define :query,                                     default: nil,   description: 'Query to use in order to limit the data extracted from the index, default nil'
+Settings.define :restore_index,                             default: nil,   description: 'Restore the given index from --dump_file, default is nil'
+Settings.define :create_index,                              default: nil,   description: 'Create an index of the given name, default is nil'
+Settings.define :duplicate_index,                           default: nil,   description: 'Duplicate the given index, defaults to nil'
+Settings.define :restore_index,                             default: nil,   description: 'Restore the given index, defaults to nil'
+Settings.define :cardinality,                               default: nil,   description: 'Return the cardinality of the given index, defaults to nil'
+Settings.define :warmers,                                   default: false, description: 'Use warmers, defaults to false'
+Settings.define :new_warmers_name,                          default: nil,   description: 'Name of warmer to create, defaults to nil'
+Settings.define :create_warmer,                             default: nil,   description: 'Query to create warmer, defaults to nil'
+Settings.define :remove_warmer,                             default: nil,   description: 'Name of warmer to remove, defaults to nil'
+Settings.define :execute_slow_queries,                      default: nil,   description: 'Execute the slow log queries in the given log file, defaults to nil'
+Settings.define :batch_size,                                default: 100,   description: 'Batch size when processing gzip file, defaults to 100'
+Settings.define :dump_mapping,                              default: nil,   description: 'The name of the file in which to dump the indexes mapping, defaults to nil'
+Settings.define :restore_mapping,                           defualt: nil,   description: 'The mapping json file to use when restoring the index, defaults to nil'
+Settings.define :duplicate_mapping,                         default: nil,   description: 'The mapping json file to use when duplicating documents in an index, defaults to nil'
+Settings.define :host,                                      default: nil,   description: 'The elasticsearch hostname, defaults to nil'
+Settings.define :port,                                      default: 9200,  description: 'The port on the elasticsearch host to connect to, defaults to 9200'
+Settings.define :logfile,                                   default: nil,   description: 'Logfile to use for slow logs, default nil'
+Settings.define :clear_all_cache,                           default: false, description: 'Clear all caches, defaults to false'
+Settings.define :clear_filter_cache,                        default: false, description: 'Clear filter cache, defaults to false'
+Settings.define :clear_fielddata,                           default: false, description: 'Clear fielddata, defaults to false'
+Settings.define :settings_index,                            default: nil,   description: 'The index that the settings listed in index_settings will be changed for, defaults to nil'
+Settings.define :es_index_settings,         :type => Array, default: [],    description: 'A comma deliminated list of elasticsearch index settings to be set for --settings_index, defaults to []'
+Settings.define :es_index_settings_values,  :type => Array, default: [],    description: 'A comma deliminated list of elasticsearch index settings values to be set for --settings_index, defaults to []'
 Settings.resolve!
 
 
@@ -59,27 +62,28 @@ class SQAR
 
     @restore_index = options[:restore_index]
     @cardinality = options[:cardinality]
-    @cache = options[:cache]
-    @temperature = options[:temperature]
     @warmers = options[:warmers]
-
     @new_warmers_name = options[:new_warmers_name]
     @create_warmer = options[:create_warmer]
-    @remove_warmer = options[:remove_warmer]
-    @refresh_interval = options[:refresh_interval]
-    @execute_slow_queries = options[:execute_slow_queries]
 
+    @remove_warmer = options[:remove_warmer]
+    @execute_slow_queries = options[:execute_slow_queries]
     @batch_size = options[:batch_size]
     @dump_mapping = options[:dump_mapping]
     @restore_mapping = options[:restore_mapping]
+
     @duplicate_mapping = options[:duplicate_mapping]
     @host = options[:host]
-
     @port = options[:port]
     @logfile = options[:logfile]
     @clear_all_cache = options[:clear_all_cache]
+
     @clear_filter_cache = options[:clear_filter_cache]
     @clear_fielddata = options[:clear_fielddata]
+    @settings_index = options[:settings_index]
+    @settings = options[:es_index_settings]
+    @settings_values = options[:es_index_settings_values]
+
   end
 
   def is_not_nil?(param)
@@ -97,21 +101,19 @@ class SQAR
   def build_task_controllers
     @some_option_names = %w[dump_index dump_mapping restore_index restore_mapping create_index duplicate_index
         duplicate_mapping restore_index cardinality new_warmers_name remove_warmer warmers create_warmer
-        execute_slow_query logfile temperature clear_all_cache clear_filter_cache clear_fielddata cache refresh_interval]
+        execute_slow_query logfile clear_all_cache clear_filter_cache clear_fielddata settings_index settings
+        settings_values]
     #puts "\n"
     #puts @some_option_names.inspect
-    @tasks = %w[backup backup restore restore restore restore duplicate duplicate cardinality warmer warmer warmer
-                replay replay cache cache cache cache index_settings index_settings refresh_interval]
+    @tasks = %w[backup backup restore restore restore restore duplicate duplicate cardinality warmer warmer warmer warmer
+                replay replay cache cache cache index_settings index_settings index_settings]
     #puts @tasks.inspect
     @base_tasks_params = {:output_dir => @output_dir, :batch_size => @batch_size, :port => @port, :host => @host}
-    #puts [@dump_index, @dump_mapping, @restore_index, @restore_mapping, @create_index, @duplicate_index,
-    #      @duplicate_mapping, @restore_index, @cardinality, @new_warmers_name, @remove_warmer, @warmers,
-    #      @create_warmer, @execute_slow_queries, @logfile, @temperature, @clear_all_cache,
-          #@clear_fielddata, @clear_filter_cache, @cache, @refresh_interval].inspect
+
     @task_controllers = [@dump_index, @dump_mapping, @restore_index, @restore_mapping, @create_index, @duplicate_index,
                          @duplicate_mapping, @restore_index, @cardinality, @new_warmers_name, @remove_warmer, @warmers,
-                         @create_warmer, @execute_slow_queries, @logfile, @temperature, @clear_all_cache,
-                         @clear_fielddata, @clear_filter_cache, @cache, @refresh_interval].zip(@some_option_names, @tasks)
+                         @create_warmer, @execute_slow_queries, @logfile, @clear_all_cache, @clear_fielddata,
+                         @clear_filter_cache, @settings_index, @settings, @settings_values].zip(@some_option_names, @tasks)
     #puts "\n"
     #@task_controllers.each do |pairs|
     #  puts pairs.inspect
@@ -137,7 +139,7 @@ class SQAR
     puts @execute_tasks.inspect
   end
 
-  def determine_warmer_action(options)
+  def determine_warmer_action(options = {})
     if is_not_nil?(options[:remove_warmer])
       options[:action] = "remove_warmer"
       options[:warmer_name] = options[:remove_warmer]
@@ -156,6 +158,21 @@ class SQAR
         options[:query] = options[:create_warmer]
         WarmerInterface.new(options).add_warmer
       end
+    end
+  end
+
+  def determine_cache_clear(options = {})
+    if options[:clear_all_cache]
+      options[:type] = "all"
+      ClearESCaches.new(options).run
+    end
+    if options[:clear_filter_cache]
+      options[:type] = "filter"
+      ClearESCaches.new(options).run
+    end
+    if options[:clear_fielddata]
+      options[:type] = "fielddata"
+      ClearESCaches.new(options).run
     end
   end
 
@@ -181,10 +198,11 @@ class SQAR
         when :replay
           Replay.new(options[:logfile], options[:host], options[:port])
         when :cache
-          puts "clearing the caches is not implemented from this method."
+          determine_cache_clear(options)
         when :index_settings
-          puts "changing the index settings is not implemented from this method yet"
-        else abort Settings.help("Must specify either backup, restore or duplicate.  Got <#{command}> UPDATE THIS LINE!")
+          options[:settings_and_values] = options[:es_index_settings].zip(options[:es_index_settings_values])
+          ChangeESIndexSettings.new(options).run
+        else abort Settings.help("Must specify either backup, restore, duplicate, cardinality, warmer, replay, cache or index_settings.  Got <#{command}> UPDATE THIS LINE!")
       end
     end
   end
