307ee37b104aba51bfb0993e553928691365abc4
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-26 11:31:00 -0500

    class to change es index settings

---
+++ test/change_es_index_settings.rb
@@ -0,0 +1,18 @@
+class ChangeESIndexSettings
+  def initialize(options = {})
+    @host = options[:host]
+    @port = options[:port]
+    @index = options[:index]
+    @settings_and_values = options[:settings_and_values]
+  end
+
+  def change_setting(setting, value)
+    `curl -XPUT 'http://#{@host}:#{@port}/#{@index}/_settings?pretty=true' -d '{ "#{setting}":"#{value}" }'`
+  end
+
+  def run
+    @settings_and_values.each do |setting, value|
+      change_setting(setting, value)
+    end
+  end
+end
