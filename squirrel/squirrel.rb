8c6a3f0f6ae7419fedbab51b79160cbe0288e6f1
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-29 16:55:29 -0500

    most recent changes

--- squirrel/squirrel.rb
+++ squirrel/squirrel.rb
@@ -13,6 +13,8 @@
 #    ruby squirrel.rb --host=localhost --port=9200 --output_dir="." --restore_file=flight_count_20130405.gz --restore_index=flight_count_20130405 --restore_mapping=flight_count_20130405_mapping.json --batch_size=100
 # duplicate files in an index from a dumpfile(duplicate_file)
 #    ruby squirrel.rb --host=localhost --port=9200 --output_dir="." --duplicate_file=flight_count_20130405.gz --duplicate_index=eight_flight_count_20130405 --duplicate_mapping=flight_count_20130405_mapping.json --batch_size=100
+# add warmer
+#    ruby squirrel.rb --host=localhost --port=9200 --output_dir="." --new_warmers_name=polite_warmer --create_warmer="curl -XGET 'localhost:9200/flight_count_20130408/flight_count/_search?pretty=true&preference=_primary_first&size=0' -d '{"sort" : ["_state", "flight_id","metric", "tb_h", "feature", "seconds", "base_feature", "metric_feature", "cnt", "_score"],"query":{"match_all":{}}}}'"
 
 
 require "configliere"
