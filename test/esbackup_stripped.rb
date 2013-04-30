c17ecc041aff8c2dc62a009885ea6d490ac1f038
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-30 14:02:22 -0500

    removed commented out text related to configliere

--- test/esbackup_stripped.rb
+++ test/esbackup_stripped.rb
@@ -6,37 +6,7 @@ require 'tire'
 require 'zlib'
 require 'socket'
 require 'pathname'
-require 'configliere'
 require 'multi_json'
-#
-#Settings.use :commandline
-#def Settings.available_commands() %w[ backup restore ]                        ; end
-#def Settings.usage()   'usage: esbackup command [ path ] [..--param=value..]' ; end
-#Settings.description = <<-DESC.gsub(/^ {2}/, '').chomp
-#  Simple backup and restore tool for Elasticsearch.
-#
-#  # Example backup command
-#  $ esbackup backup -c localhost -p 9200 -b 100 -i my_index -q '{"query":{"term":{"field":"search_term"}}}'
-#
-#  # Example restore command
-#  $ esbackup restore -c localhost -p 9200 -i my_index -m mapping_file.json
-#
-#  CAVEAT: Due to stupid fucking restrictions in the gem we use (tire), if you would like to use a dynamic query
-#  when backing up you have to specify it like so: -q '{"query_method":["*args_to_method"]}'
-#
-#  Available commands:
-##{Settings.available_commands.map{ |cmd| '    ' << cmd }.join("\n")}
-#DESC
-#Settings.define :host,       default: Socket.gethostname, flag: 'c', description: 'Host to connect to Elasticsearch'
-#Settings.define :port,       default: 9200,               flag: 'p', description: 'Port to connect to Elasticsearch'
-#Settings.define :batch_size, default: 10000,              flag: 'b', description: 'Batch size'
-#Settings.define :index,                                   flag: 'i', description: 'Index to backup/restore', required: true
-#Settings.define :mappings,                                flag: 'm', description: 'Dump mappings to or load mappings from provided file'
-#Settings.define :query,                                   flag: 'q', description: 'A JSON hash containing a query to apply to the command (backup only)'
-#Settings.define :dump_file,  default: nil,                flag: 'd', description: 'The name of the JSON dump file, if not passed the index name will be the dump files name'
-#Settings.resolve!
-#
-#Tire::Configuration.url "http://#{Settings[:host]}:#{Settings[:port]}"
 
 class ESBackup
 
@@ -180,11 +150,4 @@ class ESDup
     puts "#{@index} restore complete with #{count} records loaded"
   end
 end
-#
-#case command = Settings.rest.shift.to_s.to_sym
-#  when :restore then ESRestore.new(Settings.rest.shift, Settings.to_hash).run
-#  when :backup  then ESBackup.new(Settings.rest.shift, Settings.to_hash).run
-#  when :duplicate then ESDup.new(Settings.rest.shift, Settings.to_hash).run
-#  else abort Settings.help("Must specify either backup, restore or duplicate.  Got <#{command}>")
-#end
 
