06521a8d3497c0eb8834cdea58e0d150c88409bf
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-30 13:26:23 -0500

    removed debugging from enabling and disabling warmers

--- test/warmer_interface.rb
+++ test/warmer_interface.rb
@@ -33,25 +33,19 @@ class WarmerInterface
   def enable_warmer
     puts "closing #{@index}"
     response = `curl -s -XPOST '#{@host}:#{@port}/#{@index}/_close'`
-    puts response
     puts "enabling warmer"
     response = `curl -s -XPUT '#{@host}:#{@port}/#{@index}/_settings?pretty=true' -d '{"index.warmer.enabled":"true"}'`
-    puts response
     puts "opening #{@index}"
     response = `curl -s -XPOST '#{@host}:#{@port}/#{@index}/_open'`
-    puts response
   end
 
   def disable_warmer
     puts "closing #{@index}"
     response = `curl -s -XPOST '#{@host}:#{@port}/#{@index}/_close'`
-    #puts response
     puts "disabling warmer"
     response = `curl -s -XPUT '#{@host}:#{@port}/#{@index}/_settings?pretty=true' -d '{"index.warmer.enabled":"false"}'`
-    #puts response
     puts "opening #{@index}"
     response =`curl -s -XPOST '#{@host}:#{@port}/#{@index}/_open'`
-    #puts response
   end
 
   def determine_interaction
