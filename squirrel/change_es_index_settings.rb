cf77d674cacea506b3189e0d3eccea0902469433
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-30 14:08:22 -0500

    moved change_es_index_settings

---
+++ squirrel/change_es_index_settings.rb
@@ -0,0 +1,19 @@
+class ChangeESIndexSettings
+  def initialize(options = {})
+    @host = options[:host]
+    @port = options[:port]
+    @index = options[:index]
+    @settings_and_values = options[:settings_and_values]
+  end
+
+  def change_setting(setting, value)
+    puts "changing setting #{setting} to value #{value}"
+    `curl -s -XPUT 'http://#{@host}:#{@port}/#{@index}/_settings?pretty=true' -d '{ "#{setting}":"#{value}" }'`
+  end
+
+  def run
+    @settings_and_values.each do |setting, value|
+      change_setting(setting, value)
+    end
+  end
+end
