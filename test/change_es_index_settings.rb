53e9cc29d4a51e45b5f67124b08b7e8bf52d7780
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-30 13:50:29 -0500

    removed unneeded debugging info

--- test/change_es_index_settings.rb
+++ test/change_es_index_settings.rb
@@ -7,14 +7,11 @@ class ChangeESIndexSettings
   end
 
   def change_setting(setting, value)
-    puts "changing setting #{setting} to value #{@value}"
-    puts "curl -s -XPUT 'http://#{@host}:#{@port}/#{@index}/_settings?pretty=true' -d '{ \"#{setting}\":\"#{value}\" }'"
-    response = `curl -s -XPUT 'http://#{@host}:#{@port}/#{@index}/_settings?pretty=true' -d '{ "#{setting}":"#{value}" }'`
-    puts response
+    puts "changing setting #{setting} to value #{value}"
+    `curl -s -XPUT 'http://#{@host}:#{@port}/#{@index}/_settings?pretty=true' -d '{ "#{setting}":"#{value}" }'`
   end
 
   def run
-    puts "settings_and_values: #{@settings_and_values.inspect}"
     @settings_and_values.each do |setting, value|
       puts "setting: #{@setting} and value: #{@value}"
       change_setting(setting, value)
