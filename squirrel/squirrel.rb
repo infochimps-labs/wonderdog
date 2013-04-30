69c4bb3457ff07d5544127e5942bc3f432614f49
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-30 14:07:38 -0500

    changed require to reflect move of esbackup_stripper

--- squirrel/squirrel.rb
+++ squirrel/squirrel.rb
@@ -30,7 +30,7 @@
 
 require "configliere"
 require "multi_json"
-require_relative "../test/esbackup_stripped.rb"
+require_relative "../squirrel/esbackup_stripped.rb"
 require_relative "../squirrel/replay.rb"
 require_relative "../test/warmer_interface.rb"
 require_relative "../squirrel/clear_es_caches.rb"
