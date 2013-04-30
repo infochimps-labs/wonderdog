c28aec3295f4ab6b70751e1585174371288a1129
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-30 14:05:51 -0500

    moved clear_es_caches

---
+++ squirrel/clear_es_caches.rb
@@ -0,0 +1,30 @@
+class ClearESCaches
+  def initialize(options={})
+    @to_clear = options[:type]
+    @host = options[:host]
+    @port = options[:port]
+  end
+
+  def clear_all
+    `curl -s -XPOST 'http://#{@host}:#{@port}/_all/_cache/clear?field_data=true&filter=true&bloom=true' ; echo`
+  end
+
+  def clear_filter_cache
+    `curl -s -XPOST 'http://#{@host}:#{@port}/_all/_cache/clear?field_data=false&filter=true&bloom=true' ; echo`
+  end
+
+  def clear_fielddata
+    `curl -s -XPOST 'http://#{@host}:#{@port}/_all/_cache/clear?field_data=true&filter=false&bloom=true' ; echo`
+  end
+
+  def run
+    puts @to_clear
+    case command = @to_clear.to_sym
+      when :all then clear_all
+      when :filter then clear_filter_cache
+      when :fielddata then clear_fielddata
+      else abort "#{command} not recognized"
+    end
+  end
+
+end
