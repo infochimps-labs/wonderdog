bc47e310174a16a9f5f7de7d52b35507255446fd
Missy <missy@missy-Lotus-P150EM.(none)>
2013-04-30 14:28:32 -0500

    removed unneeded function definition

--- squirrel/squirrel.rb
+++ squirrel/squirrel.rb
@@ -116,10 +116,6 @@ class Squirrel
 
   end
 
-  def is_not_nil?(param)
-    !param.nil?
-  end
-
   def build_task_controllers
     @some_option_names = %w[dump_index dump_mapping restore_file restore_index restore_mapping duplicate_file
         duplicate_index duplicate_mapping cardinality card_file new_warmers_name warmers_index remove_warmer warmers
